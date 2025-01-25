c78868d3cdd74d34fb9ac87ee609eafc28fb6237
songliyu <songliyu@tencent.com>
2025-01-26 00:21:18 +0800

    Hack: use builtin tmux command in ~/.tmux.conf, and this repo is only for script usage.

--- scripts/cpu_info.sh
+++ scripts/cpu_info.sh
@@ -50,7 +50,7 @@ get_load() {
 
 main() {
   # storing the refresh rate in the variable RATE, default is 5
-  RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
+  RATE=$(get_tmux_option "@dracula-refresh-rate" 1)
   cpu_load=$(get_tmux_option "@dracula-cpu-display-load" false)
   cpu_label=$(get_tmux_option "@dracula-cpu-usage-label" "CPU")
   if [ "$cpu_load" = true ]; then
